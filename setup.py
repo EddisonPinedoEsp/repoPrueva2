20445da54354be91090c9717bd3b000c4588d565
EddisonPinedoEsp <jose.pinedo@utec.edu.pe>
2025-04-13 07:00:57 -0500

    Add untracked files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repoPrueva2",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Edd_pe",                         # Tu nombre
+    author_email="jose.pinedo@utec.edu.pe",                 # Tu correo electrónico
+    url="https://github.com/EddisonPinedoEsp/repoPrueva2",     # URL del proyecto
+)
